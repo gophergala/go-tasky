b2419f27701ce31ad10eaabe9928f62ed9798b3c
Mehul Choube <mchoube@gmail.com>
2015-01-24 09:43:47 -0800

    updated the Worker interface.

---
+++ tasky/tasky.go
@@ -0,0 +1,44 @@
+package tasky
+
+type Action uint64
+
+const (
+	Cancel Action = iota
+	Pause
+	Resume
+	Rerun
+)
+
+type Worker interface {
+	// Description of the worker and it's usage
+	Info() string
+
+	// List of available tasks it can service
+	Services() string
+
+	// Execute the task
+	Perform() (string, bool)
+
+	// Worker status
+	Status() string
+
+	// Action to be taken on ongoing task
+	Signal(Action) bool
+
+	// Worker statistics like number of tasks performed, failure rate,
+	// avaerage time per task etc
+	Statistics() string
+}
+
+type WorkerInst struct {
+	// SHA1 Id
+	Id string
+}
+
+func Register(Worker) (*WorkerInst, error) {
+	return nil, nil
+}
+
+func (w *WorkerInst) ListenAndServe() error {
+	return nil
+}
