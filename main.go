1c1789906e1c1d86f2a49b1d2155d2b520d3bf3b
MarkMoudy <mark.moudy@ericsson.com>
2015-01-24 12:46:05 -0600

    added basic workers route and simple router structure

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import ()
+import (
+	"github.com/gophergala/go-tasky/tasky"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+var (
+	apiBase string
+)
+
+func init() {
+	apiBase = "/tasky/v1"
+}
 
 func main() {
-	println("go-testy Hello World!")
+	println("go-tasky Started!")
+
+	workerStore := tasky.Workers{
+		Store: map[string]*tasky.Worker{},
+	}
+
+	unameWorker := tasky.Worker{
+		ID:          "",
+		Name:        "uname",
+		Description: "fetch operating system name",
+	}
+	workerStore.CreateWorker(&unameWorker)
+
+	ifconfigWorker := tasky.Worker{
+		ID:          "",
+		Name:        "ifconfig",
+		Description: "fetch network configuration",
+	}
+	workerStore.CreateWorker(&ifconfigWorker)
+
+	mainRouter := mux.NewRouter()
+
+	//handles /tasky/v1 routes.  Create new sub-routers off of this
+	taskySubRtr := mainRouter.PathPrefix(apiBase).Subrouter()
+
+	//handles /tasky/v1/workers routes
+	workersSubRtr := taskySubRtr.PathPrefix("/workers").Subrouter()
+
+	workersSubRtr.HandleFunc("/", workerStore.Index).Methods("GET")
+
+	http.ListenAndServe(":4444", mainRouter)
+
 }
--- tasky/workers.go
+++ tasky/workers.go
@@ -1,7 +1,52 @@
 package tasky
 
-import ()
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+)
 
 //Worker is an interface for defining a worker type.  All custom workers must implement this interface.
-type Worker interface {
+type Workers struct {
+	sync.RWMutex
+	Store map[string]*Worker
+}
+
+type Worker struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+func (workers *Workers) CreateWorker(worker *Worker) {
+	workers.Lock()
+	id := fmt.Sprintf("%d", len(workers.Store)) //replace this with database backing id, temp for now
+	worker.ID = id
+	workers.Store[id] = worker
+	workers.Unlock()
+}
+
+func (workers *Workers) Index(w http.ResponseWriter, req *http.Request) {
+	if len(workers.Store) == 0 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	RespondJSON(w, req, workers.Store)
+
+}
+
+func RespondJSON(w http.ResponseWriter, req *http.Request, v interface{}, code ...int) error {
+	if code != nil {
+		w.WriteHeader(code[0])
+	}
+	err := json.NewEncoder(w).Encode(v)
+	if err != nil {
+		//encoding failed
+		panic(err)
+	}
+
+	return err
 }
