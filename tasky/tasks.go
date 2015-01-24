f33b872bca4a665d969c64f6c56ec2b959759b9a
MarkMoudy <mark.moudy@ericsson.com>
2015-01-24 02:42:53 -0600

    project structure

---
+++ tasky/tasks.go
@@ -0,0 +1,7 @@
+package tasky
+
+import ()
+
+//Task is an interface for defining a task type.  All custom workers must have a task that implements this interface.
+type Task interface {
+}
---
+++ tasky/workers.go
@@ -0,0 +1,7 @@
+package tasky
+
+import ()
+
+//Worker is an interface for defining a worker type.  All custom workers must implement this interface.
+type Worker interface {
+}
